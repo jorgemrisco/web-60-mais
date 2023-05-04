c425b4c4f1eba50bad8d758236b2aaf8ef08a02b
Jorge Marcelo Risco <jorgedelano7@gmail.com>
2023-05-04 13:40:20 -0300

    Página de testes inscrição

---
+++ src/pages/teste-inscricao.tsx
@@ -0,0 +1,18 @@
+export default function TesteInscricaoSucesso() {
+  return (
+    <div
+      style={{
+        height: "90vh",
+        display: "flex",
+        alignContent: "center",
+        alignSelf: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Inscrição realizada com sucesso</h1>
+      <p>Página de testes</p>
+      <h1>Inscrição realizada com sucesso</h1>
+    </div>
+  );
+}
